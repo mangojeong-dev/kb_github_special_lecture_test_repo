3378042add7ee86e7bb8ffa4df72251032c36d06
mangojeong <wkdgnl123@cau.ac.kr>
2026-04-24 10:40:05 +0900

    feat : Dummy 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,4 +8,18 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	public void sayHello() {
+		System.out.println("Hello from Demo1Application!");
+	}
+
+	public int addNumbers(int a, int b) {
+		return a + b;
+	}
+
+	public String getStatus() {
+		return "Application is running";
+	}
+
 }
+
