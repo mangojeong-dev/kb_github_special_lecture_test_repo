957ba037cd5994b94070639e65a4283c57716444
mangojeong <wkdgnl123@cau.ac.kr>
2026-04-24 11:30:57 +0900

    feat : 파일 추가

---
+++ src/main/java/com/example/demo/DemoApplication1.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication1 {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication1.class, args);
+	}
+
+	public void sayHello() {
+		System.out.println("Hello from Demo1Application!");
+	}
+
+	public int addNumbers(int a, int b) {
+		return a + b;
+	}
+
+	public String getStatus() {
+		return "Application is running";
+	}
+	public String getStatus1() {
+		return "Application is running";
+	}
+
+}
+
