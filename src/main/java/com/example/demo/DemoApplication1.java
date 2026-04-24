433fc8a9503693e5f2a0500633208a241f76672d
mangojeong <wkdgnl123@cau.ac.kr>
2026-04-24 13:18:54 +0900

    feat : a 기능 개발 완료

--- src/main/java/com/example/demo/DemoApplication1.java
+++ src/main/java/com/example/demo/DemoApplication1.java
@@ -5,27 +5,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication1 {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication1.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication1.class, args);
+    }
 
-	public void sayHello() {
-		System.out.println("Hello from Demo1Application!");
-	}
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
 
-	public int addNumbers(int a, int b) {
-		return a + b;
-	}
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
 
-	public String getStatus() {
-		return "Application is running";
-	}
-	public String getStatus1() {
-		return "Application is running";
-	}
+    public String getStatus() {
+        return "Application is running";
+    }
 
-	public String getStatus2() {
-		return "Application is running";
+    public String getStatus1() {
+        return "Application is running";
+    }
 
+    public String getStatus2() {
+        return "Application is running";
+    }
+
+    public String b() {
+        return "Application is running";
+
+    }
 }
-
